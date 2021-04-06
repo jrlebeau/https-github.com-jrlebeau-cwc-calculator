da362343a624bf61e9f598173a94e3a2f7d62128
Jim LeBeau <jrlebeau@yahoo.com>
2021-04-06 12:37:06 -0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by James Lebeau on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
